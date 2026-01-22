a822b63bcfac2256333aba46fdab971d540bd136
mneno1 <mnenoi.mobile@gmail.com>
2026-01-22 15:20:16 +0900

    Add files via upload

---
+++ qmk_firmware/keyboards/lotus_qaz/keymaps/vial/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xeb, 0xf0, 0xc8, 0x1b, 0x72, 0xba, 0x7b, 0xc9}
+#define VIAL_TAP_DANCE_ENTRIES 8
+#define VIAL_COMBO_ENTRIES 8
+#define PICO_FLASH_SIZE_BYTES (1 * 1024 * 1024)
+#define POINTING_DEVICE_ROTATION_90
